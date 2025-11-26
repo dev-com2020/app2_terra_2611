f896f6887c6566ac21ae09c8f69094e5ca33c439
DEV-COM <kwp@tlen.pl>
2025-11-26 12:12:32 +0100

    Add network configuration and outputs to Terraform files

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 locals {
     project_name = "my-terraform-project"
     dynamic_name = "${var.env_name}-${random_string.random.result}"
+    network_list = [
+        {name = "net-a", cidr = "10.0.0.0/16"},
+        {name = "net-b", cidr = "10.1.0.0/16"}
+    ]
+    network_map = {
+        "westus" = "net-a"
+        "eastus" = "net-b"
+    }
 }
 
 resource "random_string" "random" {
--- variables.tf
+++ variables.tf
@@ -2,32 +2,32 @@ variable "env_name" {
   type = string
 }
 
-variable "zmienna1" {
-  type = number
-}
+# variable "zmienna1" {
+#   type = number
+# }
 
-variable "zmienna2" {
-  type = bool
-}
+# variable "zmienna2" {
+#   type = bool
+# }
 
-variable "zmienna3" {
-  type = list(any)
-}
+# variable "zmienna3" {
+#   type = list(any)
+# }
 
-variable "zmienna4" {
-  type = map(any)
-}
+# variable "zmienna4" {
+#   type = map(any)
+# }
 
-variable "zmienna5" {
-  type = object({
-    a = string
-    b = number
-    c = bool
-    d = list(string)
-    e = object({
-      one = string
-      two = number 
-    })
-  })
+# variable "zmienna5" {
+#   type = object({
+#     a = string
+#     b = number
+#     c = bool
+#     d = list(string)
+#     e = object({
+#       one = string
+#       two = number 
+#     })
+#   })
   
-}
+# }
