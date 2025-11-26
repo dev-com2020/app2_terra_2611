dfb815b8e4982fc75b23d4ce3833fdfca8429e68
DEV-COM <kwp@tlen.pl>
2025-11-26 12:02:28 +0100

    Add additional Terraform variables for configuration

--- variables.tf
+++ variables.tf
@@ -1,3 +1,33 @@
 variable "env_name" {
   type = string
 }
+
+variable "zmienna1" {
+  type = number
+}
+
+variable "zmienna2" {
+  type = bool
+}
+
+variable "zmienna3" {
+  type = list(any)
+}
+
+variable "zmienna4" {
+  type = map(any)
+}
+
+variable "zmienna5" {
+  type = object({
+    a = string
+    b = number
+    c = bool
+    d = list(string)
+    e = object({
+      one = string
+      two = number 
+    })
+  })
+  
+}
