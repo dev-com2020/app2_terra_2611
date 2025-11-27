d9645ed8a6c937814c2495eb070bc4e07f047433
DEV-COM <kwp@tlen.pl>
2025-11-27 12:17:45 +0100

    Dodaj moduły EC2 dla instancji aplikacji i bazy danych z odpowiednimi zmiennymi

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0a6793a25df710b06"
-  instance_type = "t2.nano"
+module "app_server" {
+    source = "./modules/ec2_instance"
+    
+    instance_name = "tk_app-server"
+    instance_type = "t2.micro"
+    ami           = "ami-0a6793a25df710b06" # Amazon Linux 2 AMI EU-central-1
+}
 
-  tags = {
-    Name = "TK_AppServerInstance"
-  }
-  
+module "db_server" {
+    source = "./modules/ec2_instance"
+    
+    instance_name = "tk_db-server"
+    instance_type = "t2.micro"
+    ami           = "ami-05a797deb2b0220d6" # Amazon Linux 2 AMI EU-central-1
 }
---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+    description = "The type of instance to use"
+    type        = string
+    default     = "t2.micro"
+}
+
+variable "ami" {
+    description = "The AMI to use for the instance"
+    type        = string
+}
+
+variable "instance_name" {
+    description = "The name tag for the instance"
+    type        = string
+    default     = "TK_AppServerInstance"
+}
