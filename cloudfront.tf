03b74591ee33d9969c651ea2e581f3566cbf4716
DEV-COM <kwp@tlen.pl>
2025-11-27 12:06:36 +0100

    Add CloudFront distribution configuration for S3-backed static website

---
+++ cloudfront.tf
@@ -0,0 +1,42 @@
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.website_bucket.id}"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "S3-backed static website"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.website_bucket.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
