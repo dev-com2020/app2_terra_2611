363f8d58f0676579e3d889642e0b14c662c56741
DEV-COM <kwp@tlen.pl>
2025-11-28 11:58:51 +0100

    Dodaj zasoby Terraform dla koszyka S3 do CI/CD

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "ci_cd_bucket" {
+  bucket = "tk00-my-terraform-ci-cd-bucket-example" 
+
+  tags = {
+    Name        = "tk_CiCdBucket"
+    ManagedBy   = "tk_GitHubActions"
+  }
+}
